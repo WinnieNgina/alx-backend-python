76b5b2c101b3e759ac95eab5a7655124e355fb32
Winfred <winfrednginakilonzo@gmail.com>
2023-12-07 18:34:04 +0300

    Fix data types

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""Unions and tupple"""
-from typing import List, Union
+"""Unions and tuple"""
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
-    """Returns a tupple"""
-    return (k, v**2)
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple"""
+    return (k, float(v**2))
