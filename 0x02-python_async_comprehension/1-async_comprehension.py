304c4bd7f9123f3c7e0eb52b31f26eb7217d4d92
Winfred <winfrednginakilonzo@gmail.com>
2023-12-13 15:56:14 +0300

    Use async comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Use comprehensions"""
+import asyncio
+from typing import Generator
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> Generator[float, None, None]:
+    """return the 10 random numbers"""
+    result = [i async for i in async_generator()]
+    return result
