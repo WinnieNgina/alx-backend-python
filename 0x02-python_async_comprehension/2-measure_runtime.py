83f96d60a1ae34b179470e228fa3ef05312971d7
Winfred <winfrednginakilonzo@gmail.com>
2023-12-13 17:12:06 +0300

    Fixed list comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,14 +8,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Execute async_comprehension four times in parallel"""
     start_time = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
     end_time = time.time()
-
-    # Returning the total runtime
     return end_time - start_time
