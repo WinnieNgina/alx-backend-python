6d7eafc3df156490f01ca5824ee94bf399c86af3
Winfred <winfrednginakilonzo@gmail.com>
2024-01-08 20:42:50 +0300

    Fix mock class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
-from unittest.mock import patch, Mock
+from unittest.mock import Mock, patch
 from utils import get_json
 
 
@@ -30,17 +30,18 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """Test get_json method"""
-    @patch('utils.requests.get')
+
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
     def test_get_json(self, test_url, test_payload):
         """Mock HTTP calls"""
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        with patch('utils.requests.get',
-                   return_value=mock_response) as mock_get:
-            result = get_json(test_url)
-            mock_get.assert_called_once_with(test_url)
-            self.assertEqual(result, test_payload)
+        class MockResponse(Mock):
+            """Mocked class"""
+            def json(self):
+                """Json mocked class"""
+                return test_payload
+        with patch('requests.get') as MockClass:
+            MockClass.return_value = MockResponse()
+            self.assertEqual(get_json(test_url), test_payload)
