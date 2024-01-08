af8119d1b5d4cf24495235b5dd143912b85e1a28
Winfred <winfrednginakilonzo@gmail.com>
2024-01-08 11:47:44 +0300

    Trird fixing error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,10 +17,12 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected_result)
 
     @parameterized.expand([
-        ({}, ("a",), KeyError("'a'")),
-        ({"a": 1}, ("a", "b"), KeyError("'b'")),
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b'"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception):
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
         """Test exception"""
-        with self.assertRaises(expected_exception):
+        with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
+
+        self.assertEqual(str(context.exception), expected_exception_message)
